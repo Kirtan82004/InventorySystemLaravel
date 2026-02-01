aad503dac1a844e5ef211f1c0c407f27bf1631b3
Kirtan82004 <kirtankumar9310136575@gmail.com>
2026-02-01 15:52:44 +0530

    add cors origin

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => ['http://localhost:5173', 'https://inventory-system-silk-theta.vercel.app'],
 
     'allowed_origins_patterns' => ['/localhost:5173$/', '/127\.0\.0\.1:5173$/'],
 
